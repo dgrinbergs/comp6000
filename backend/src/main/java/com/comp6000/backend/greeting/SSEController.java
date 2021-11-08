8653c37d70958a9140c329192dd3bb014d2969a8
dgrinbergs <www.dainis.lv@gmail.com>
2021-11-08 19:42:15 +0000

    testing streams

---
+++ backend/src/main/java/com/comp6000/backend/greeting/SSEController.java
@@ -0,0 +1,20 @@
+package com.comp6000.backend.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
+
+@RestController
+public class SSEController {
+
+  @GetMapping(value = "/stream", produces = TEXT_EVENT_STREAM_VALUE)
+  public Flux<String> timestampStream() {
+    return Flux.interval(Duration.ofSeconds(1)).map(l -> LocalTime.now().toString());
+  }
+
+}
