f9554cbfce0e469454ff6d759275154882730312
dgrinbergs <www.dainis.lv@gmail.com>
2021-11-08 19:58:33 +0000

    simplified code

--- backend/src/main/java/com/comp6000/backend/greeting/WebSocketHandler.java
+++ backend/src/main/java/com/comp6000/backend/greeting/WebSocketHandler.java
@@ -14,24 +14,12 @@ import java.time.LocalTime;
 @Component
 public class WebSocketHandler implements org.springframework.web.reactive.socket.WebSocketHandler {
 
-  private static final ObjectMapper json = new ObjectMapper();
-
-  private final Flux<String> eventFlux = Flux.generate(sink -> {
-    var event = LocalTime.now().toString();
-    try {
-      sink.next(json.writeValueAsString(event));
-    } catch (JsonProcessingException e) {
-      sink.error(e);
-    }
-  });
-
-  private final Flux<String> intervalFlux = Flux.interval(Duration.ofMillis(1000L))
-      .zipWith(eventFlux, (time, event) -> event);
+  private final Flux<String> eventFlux = Flux.interval(Duration.ofSeconds(1)).map(l -> LocalTime.now().toString());
 
   @Override
   public Mono<Void> handle(WebSocketSession session) {
     return session
-        .send(intervalFlux.map(session::textMessage))
+        .send(eventFlux.map(session::textMessage))
         .and(session.receive().map(WebSocketMessage::getPayloadAsText).log());
   }
 }
