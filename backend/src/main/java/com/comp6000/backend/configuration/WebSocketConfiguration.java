2411ff5c64676fdc36ed6c900cf15db5f6b0e595
dgrinbergs <www.dainis.lv@gmail.com>
2021-11-13 17:03:36 +0000

    configurable websocket endpoint

--- backend/src/main/java/com/comp6000/backend/configuration/WebSocketConfiguration.java
+++ backend/src/main/java/com/comp6000/backend/configuration/WebSocketConfiguration.java
@@ -2,6 +2,7 @@ package com.comp6000.backend.configuration;
 
 import com.comp6000.backend.generation.events.GenerationEventWebSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.HandlerMapping;
@@ -12,10 +13,13 @@ import java.util.Map;
 @Configuration
 public class WebSocketConfiguration {
 
+  private final String schematicWebSocketEndpoint;
   private final GenerationEventWebSocketHandler generationEventWebSocketHandler;
 
   @Autowired
-  public WebSocketConfiguration(GenerationEventWebSocketHandler generationEventWebSocketHandler) {
+  public WebSocketConfiguration(@Value("${api.ws.schematics.endpoint}") String schematicWebSocketEndpoint,
+                                GenerationEventWebSocketHandler generationEventWebSocketHandler) {
+    this.schematicWebSocketEndpoint = schematicWebSocketEndpoint;
     this.generationEventWebSocketHandler = generationEventWebSocketHandler;
   }
 
@@ -24,7 +28,7 @@ public class WebSocketConfiguration {
     SimpleUrlHandlerMapping handlerMapping = new SimpleUrlHandlerMapping();
     handlerMapping.setOrder(1);
     handlerMapping.setUrlMap(Map.of(
-        "/ws/schematics", generationEventWebSocketHandler
+        schematicWebSocketEndpoint, generationEventWebSocketHandler
     ));
     return handlerMapping;
   }
