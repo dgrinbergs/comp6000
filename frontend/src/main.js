f3cc8298f7798030663b8f51868cd35571aa8712
dgrinbergs <www.dainis.lv@gmail.com>
2021-11-24 23:55:21 +0000

    frontend docker compose

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,8 @@ import App from './App.vue'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
+import axios from "axios";
+
+axios.defaults.baseURL=`${process.env.VUE_APP_API_URL}:${process.env.VUE_APP_API_PORT}`
 
 createApp(App).mount('main')
