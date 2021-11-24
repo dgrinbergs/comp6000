9446917e0d2209754448fa975b9b6ad230eca7bc
dgrinbergs <www.dainis.lv@gmail.com>
2021-11-24 23:57:22 +0000

    removed frontend from docker-compose

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,6 +5,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import axios from "axios";
 
-axios.defaults.baseURL=`${process.env.VUE_APP_API_URL}:${process.env.VUE_APP_API_PORT}`
+axios.defaults.baseURL=process.env.VUE_APP_API_URL;
 
 createApp(App).mount('main')
