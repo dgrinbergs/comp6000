53efa7ef2654edbccc6a23e17e604bbead49c46d
sw2098 <groupcode21@>
2022-02-24 16:21:35 +0000

    Started writing the About Us page for the frontend site.

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>About Us</h1>
+  <h1>About Us</h1><br>
+  <h3>Overview</h3>
+  <div>
+    We are Final Year Computer Science Students at the University of Kent.<br>
+    Our project's aim was to create an application which creates houses and
+    buildings in Minecraft based on feedback provided by the user of a frontend webpage.<br>
+    The feedback provided powers a AI system which uses a Genetic Algorithm to create a
+    number of buildings of varying materials and block types.<br>
+    SmartGen allows users to create buildings in Minecraft without ever setting foot in the game itself.
+  </div>
+  <h3>Who we are?</h3>
+  <div>
+    <div>Samuel Wheeler<br>Project Manager</div>
+    <div>Harry Devane<br>Developer</div>
+    <div>Dainis Grinbergs<br>Developer</div>
+    <div>Corinne Batho-Newton<br>Researcher, Algorithm Logician and Administration</div>
+  </div>
 </template>
 
 <script lang="ts">
