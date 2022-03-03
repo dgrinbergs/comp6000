cd1aa5fa73db14dccf0c2c5728c9d33ac0244ce0
sw2098 <groupcode21@>
2022-03-03 19:40:57 +0000

    Started writing the About Us page for the frontend site.

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -11,10 +11,14 @@
   </div>
   <h3>Who we are?</h3>
   <div>
-    <div>Samuel Wheeler<br>Project Manager</div>
-    <div>Harry Devane<br>Developer</div>
-    <div>Dainis Grinbergs<br>Developer</div>
-    <div>Corinne Batho-Newton<br>Researcher, Algorithm Logician and Administration</div>
+    <div>
+      <img src="https://aminoapps.com/c/minecraft/page/blog/the-og-steve-head/jGuK_u8RYD3b6VzBxZnjjXBajmKJrm" alt="Minecraft Steve Head"><br>Samuel Wheeler<br>Project Manager</div>
+    <div>
+      <img src="https://aminoapps.com/c/minecraft/page/blog/the-og-steve-head/jGuK_u8RYD3b6VzBxZnjjXBajmKJrm" alt="Minecraft Steve Head"><br>Harry Devane<br>Developer</div>
+    <div>
+      <img src="https://aminoapps.com/c/minecraft/page/blog/the-og-steve-head/jGuK_u8RYD3b6VzBxZnjjXBajmKJrm" alt="Minecraft Steve Head"><br>Dainis Grinbergs<br>Developer</div>
+    <div>
+      <img src="https://aminoapps.com/c/minecraft/page/blog/the-og-steve-head/jGuK_u8RYD3b6VzBxZnjjXBajmKJrm" alt="Minecraft Steve Head"><br>Corinne Batho-Newton<br>Researcher, Algorithm Logician and Administration</div>
   </div>
 </template>
 
