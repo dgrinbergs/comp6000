794581698b9b53dc47d14ba6ec690a5943b6bfbd
dgrinbergs <www.dainis.lv@gmail.com>
2022-03-04 13:20:19 +0000

    done

--- frontend/pages/about.vue
+++ frontend/pages/about.vue
@@ -1,57 +1,45 @@
 <template>
-  <style>
-    body {
-      font-family: 'IBM Plex Sans', sans-serif;
-    }
-    #overview {
-      margin: auto;
-      width: 80%;
-      float: center;
-    }
-    #team {
-      float: center;
-      margin: auto;
-    }
-    #teammembers {
-      float: left;
-      padding: 40px;
-      text-align: center;
-      margin: auto;
-    }
-    h1 {
-      text-align: center;
-    }
-    h3 {
-      text-align: center;
-    }
-    img {
-      height: 30px;
-      width: 30px;
-    }
-  </style>
-  <h1>About Us</h1><br>
-  <h3>Overview</h3>
-  <div id="overview">
-    We are Final Year Computer Science Students at the University of Kent.<br>
-    Our project's aim was to create an application which creates houses and
-    buildings in Minecraft based on feedback provided by the user of a frontend webpage.<br>
-    The feedback provided powers a AI system which uses a Genetic Algorithm to create a
-    number of buildings of varying materials and block types.<br>
-    SmartGen allows users to create buildings in Minecraft without ever setting foot in the game itself.
-  </div>
-  <h3>Who we are?</h3>
-  <div style="margin: auto; width: 80%; float: center;">
-    <div id="teammembers">
-      <img src="https://pm1.narvii.com/7616/bc3b22a89815985dc74b830a01ad331c0d3e9f86r1-768-768v2_hq.jpg" alt="Minecraft Steve Head"><br>Samuel Wheeler<br>Project Manager
+  <div class="p-4 flex flex-col space-y-4">
+    <div>
+      <h1>About Us</h1>
+      <h3>Overview</h3>
+      <div id="overview">
+        <p>We are final year Computer Science students at the University of Kent.</p>
+        <p>Our projects aim was to create an application which creates houses and
+          buildings in Minecraft based on feedback provided by the user of a frontend webpage.</p>
+        <p>The feedback provided powers a AI system which uses a Genetic Algorithm to create a
+          number of buildings of varying materials and block types.</p>
+        <p>SmartGen allows users to create buildings in Minecraft without ever setting foot in the game itself.</p>
+      </div>
     </div>
-    <div id="teammembers">
-      <img src="https://pm1.narvii.com/7616/bc3b22a89815985dc74b830a01ad331c0d3e9f86r1-768-768v2_hq.jpg" alt="Minecraft Steve Head"><br>Harry Devane<br>Developer
-    </div>
-    <div id="teammembers">
-      <img src="https://pm1.narvii.com/7616/bc3b22a89815985dc74b830a01ad331c0d3e9f86r1-768-768v2_hq.jpg" alt="Minecraft Steve Head"><br>Dainis Grinbergs<br>Developer
-    </div>
-    <div id="teammembers">
-      <img src="https://pm1.narvii.com/7616/bc3b22a89815985dc74b830a01ad331c0d3e9f86r1-768-768v2_hq.jpg" alt="Minecraft Steve Head"><br>Corinne Batho-Newton<br>Researcher, Algorithm Logician and Administration
+    <div>
+      <h3>Who we are</h3>
+      <div id="team">
+        <div class="team-member">
+          <img src="https://pm1.narvii.com/7616/bc3b22a89815985dc74b830a01ad331c0d3e9f86r1-768-768v2_hq.jpg"
+               alt="Minecraft Steve Head">
+          <p>Samuel Wheeler</p>
+          <p>Project Manager</p>
+        </div>
+        <div class="team-member">
+          <img src="https://pm1.narvii.com/7616/bc3b22a89815985dc74b830a01ad331c0d3e9f86r1-768-768v2_hq.jpg"
+               alt="Minecraft Steve Head">
+          <p>Harry Devane</p>
+          <p>Developer</p>
+        </div>
+        <div class="team-member">
+          <img src="https://pm1.narvii.com/7616/bc3b22a89815985dc74b830a01ad331c0d3e9f86r1-768-768v2_hq.jpg"
+               alt="Minecraft Steve Head">
+          <p>Dainis Grinbergs</p>
+          <p>Developer</p>
+        </div>
+        <div class="team-member">
+          <img src="https://pm1.narvii.com/7616/bc3b22a89815985dc74b830a01ad331c0d3e9f86r1-768-768v2_hq.jpg"
+               alt="Minecraft Steve Head">
+          <p>Corinne Batho-Newton</p>
+          <p>Researcher, Algorithm Logician and Administration</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -63,3 +51,17 @@ export default Vue.extend({
   name: 'AboutPage',
 })
 </script>
+<style>
+
+#team {
+  @apply flex flex-row items-center justify-around;
+}
+
+.team-member {
+  @apply flex flex-col justify-center items-center;
+}
+
+.team-member > img {
+  @apply h-12;
+}
+</style>
