e4dfa2ea70590689e2bfcb599ab8d823fe95cb05
dgrinbergs <www.dainis.lv@gmail.com>
2022-03-16 18:41:49 +0000

    corpus draft

---
+++ corpus/data.js
@@ -0,0 +1,41 @@
+const members = [
+  "Corinne Newton",
+  "Dainis Grinbergs",
+  "Harry Devane",
+  "Samuel Wheeler",
+];
+
+const documents = [
+  {
+    name: "User documentation",
+    href: "#",
+  },
+  {
+    name: "Testing Schedules",
+    href: "#",
+  },
+  {
+    name: "Specifications",
+    href: "#",
+  },
+  {
+    name: "Meeting Minutes",
+    href: "#",
+  },
+  {
+    name: "Literature Reviews",
+    href: "#",
+  },
+  {
+    name: "Implementations",
+    href: "#",
+  },
+  {
+    name: "Designs",
+    href: "#",
+  },
+  {
+    name: "Analyses",
+    href: "#",
+  },
+];
