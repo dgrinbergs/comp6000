b98a3ad67dc9d23763fb960083fed517e223e76d
sw2098 <groupcode21@>
2022-03-22 16:54:28 +0000

    Updated code to include all documents

--- corpus/data.js
+++ corpus/data.js
@@ -7,12 +7,196 @@ const members = [
 
 const sections = [
   {
-    section: "Sample Section",
+    section: "Analyses",
     documents: [
       {
-        name: "Sample Document",
-        href: "#sample-document.pdf",
+        name: "A.I. Analysis",
+        href: "#A.I. Analysis.pdf",
       },
-    ],
+      {
+        name: "Stages of Development",
+        href: "#Stages of Development.pdf",
+      }
+    ]
   },
+  {
+    section: "Designs",
+    documents: [
+      {
+        name: "Building Generation Designs",
+        href: "#Building Generation Designs.pdf",
+      },
+      {
+        name: "Front End Designs",
+        href: "#Front End Designs.pdf",
+      },
+      {
+        name: "A.I. Design Flowchart",
+        href: "#A.I. Design Flowchart (V1).pdf",
+      },
+      {
+        name: "Backend Design (Development)",
+        href: "#backend-design-development.png",
+      },
+      {
+        name: "Backend Design (Production)",
+        href: "#backend-design-production.png",
+      },
+      {
+        name: "Genetic Algorithm Flowchart",
+        href: "#GA_Design_Flowchart_V2.pdf",
+      },
+      {
+        name: "Sequence Diagram (Feedback Loop)",
+        href: "#sequence_diagram_feedback_loop.png",
+      },
+      {
+        name: "Sequence Diagram (Generate Building)",
+        href: "#sequenc_diagram_generate_building.pdf",
+      }
+    ]
+  },
+  {
+    section: "Implementations",
+    documents: [
+      {
+        name: "Code",
+        href: "#comp6000-develop.zip",
+      }
+    ]
+  },
+  {
+    section: "Literature Reviews",
+    documents: [
+      {
+        name: "A.I. Literature Review",
+        href: "#A.I. Literature Review.pdf",
+      }
+    ]
+  },
+  {
+    section: "Meeting Minutes",
+    documents: [
+      {
+        name: "Minutes Template",
+        href: "#Meeting Minutes Template.pdf",
+      },
+      {
+        name: "Minutes #1 - 07/10/2021",
+        href: "#Minutes #1 - 07/10/2021.pdf",
+      },
+      {
+        name: "Minutes #2 - 13/10/2021",
+        href: "#Minutes #2 - 13/10/2021.pdf",
+      },
+      {
+        name: "Minutes #3 - 29/10/2021",
+        href: "#Minutes #3 - 29/10/2021.pdf",
+      },
+      {
+        name: "Minutes #4 - 03/11/2021",
+        href: "#Minutes #4 - 03/11/2021.pdf",
+      },
+      {
+        name: "Minutes #5 - 05/11/2021",
+        href: "#Minutes #5 - 05/11/2021.pdf",
+      },
+      {
+        name: "Minutes #6 - 12/11/2021",
+        href: "#Minutes #6 - 12/11/2021.pdf",
+      },
+      {
+        name: "Minutes #7 - 26/11/2021",
+        href: "#Minutes #7 - 26/11/2021.pdf",
+      },
+      {
+        name: "Minutes #8 - 10/12/2021",
+        href: "#Minutes #8 - 10/12/2021.pdf",
+      },
+      {
+        name: "Minutes #9 - 25/01/2022",
+        href: "#Minutes #9 - 25/01/2022.pdf",
+      },
+      {
+        name: "Minutes #10 - 29/01/2022",
+        href: "#Minutes #10 - 29/01/2022.pdf",
+      },
+      {
+        name: "Minutes #11 - 31/01/2022",
+        href: "#Minutes #11 - 31/01/2022.pdf",
+      },
+      {
+        name: "Minutes #12 - 01/02/2022",
+        href: "#Minutes #12 - 01/02/2022.pdf",
+      },
+      {
+        name: "Minutes #13 - 02/02/2022",
+        href: "#Minutes #13 - 02/02/2022.pdf",
+      },
+      {
+        name: "Minutes #14 - 08/02/2022",
+        href: "#Minutes #14 - 08/02/2022.pdf",
+      },
+      {
+        name: "Minutes #15 - 22/02/2022",
+        href: "#Minutes #15 - 22/02/2022.pdf",
+      },
+      {
+        name: "Minutes #16 - 01/03/2022",
+        href: "#Minutes #16 - 01/03/2022.pdf",
+      },
+      {
+        name: "Minutes #17 - 15/03/2022",
+        href: "#Minutes #17 - 15/03/2022.pdf",
+      },
+      {
+        name: "Minutes #18 - 22/02/2022",
+        href: "#Minutes #18 - 22/03/2022.pdf",
+      }
+    ]
+  },
+  {
+    section: "Specifications",
+    documents: [
+      {
+        name: "Initial Specification",
+        href: "#Initial Specification.pdf",
+      },
+      {
+        name: "Reviewed Specification",
+        href: "#Reviewed Specification.pdf",
+      }
+    ]
+  },
+  {
+    section: "Testing Schedules",
+    documents: [
+      {
+        name: "Backend Application Testing",
+        href: "#Backend Application Testing.pdf",
+      },
+    ]
+  },
+  {
+    section: "User Documentation",
+    documents: [
+      {
+        name: "Guide to Run the System",
+        href: "#Guide to Run the System.pdf",
+      },
+      {
+        name: "User Guide",
+        href: "#User Guide",
+      }
+    ]
+  },
+  {
+    section: "Template",
+    documents: [
+      {
+        name: "Document Template",
+        href: "#Document Template.pdf",
+      }
+    ]
+  }
 ];
